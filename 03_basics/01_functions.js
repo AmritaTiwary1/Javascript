f12d1181a43524ffd399c4fba88771951709b971
Amrita Tiwary <119927883+AmritaTiwary1@users.noreply.github.com>
2024-03-17 13:13:33 +0000

    function02

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,4 +30,74 @@ let res = loginUserMessage("dev");
 //console.log(loginUserMessage());  //undefined just logged in , when we dont pass any arguments , it givs undefined value
 
 //After adding "if statement" in loginUserMessage function:
-console.log(loginUserMessage());    //please enter a username            undefined
+//console.log(loginUserMessage());    //please enter a username            undefined
+
+
+//-----------------------------------------------------------------------------------
+
+
+function calculateCartPrice1(num1) { 
+   return num1;
+}
+//console.log(calculateCartPrice1(2));    //2
+//console.log(calculateCartPrice1(200,300,400));   //200
+
+
+
+
+
+//when we dont know how many argument will be passed in func bt want all of them  --- we use (...) is called  rest / spread operator
+
+function calculateCartPrice2(...num1) {  //... is rest operator here not spread operator, it says takes all arguments in one nums , value inside the nums will be in array format
+    return num1;
+}
+//console.log(calculateCartPrice2(2));    //2
+//console.log(calculateCartPrice2(200,300,400));   //[ 200, 300, 400 ]
+
+
+
+
+function calculateCartPrice3(firstVal , secondVal , ...num1) {  //... is rest operator here not spread operator, it says takes all arguments in one nums , value inside the nums will be in array format
+    console.log("first Argument --",firstVal , ", Second Argument--",secondVal);
+    return num1;
+}
+//calculateCartPrice3(2);    //first Argument -- 2 , Second Argument-- undefined
+
+//console.log(calculateCartPrice3(2));    //{ first Argument --2 , Second Argument--undefined     [] }----this square bracket in output is num1
+
+//console.log(calculateCartPrice3(200,300,400,700));  
+/* OUTPUT : 
+first Argument -- 200 , Second Argument-- 300
+[ 400, 700 ]
+*/
+
+
+//PASSING OBJECT IN FUNC--------------------------------------------------------
+
+
+const user = {
+    username : "hitesh",
+    price: 199
+}
+
+function handleObject(anyObject) {
+    console.log(`username is ${anyObject.username} and price is : ${anyObject.price}`);
+}
+//handleObject(user);   //username is hitesh and price is : 199
+
+//handleObject({
+//    username:"dev",
+//    price:"priceless"
+//})
+
+//PASSING ARRAYS IN FUNCTION
+
+const arr = [100,200,300,400];
+
+function returnSecondValue(getArray) {
+    return getArray[1];
+}
+
+console.log(returnSecondValue(arr));  //200
+
+console.log(returnSecondValue([900,500,400,500,300]));   //500
