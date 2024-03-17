2f1be0af540aa8afece0248ec59b0edf5e8947bb
Amrita Tiwary <119927883+AmritaTiwary1@users.noreply.github.com>
2024-03-17 10:14:21 +0000

    objectPart2 completed

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -6,7 +6,7 @@ const sym1 = Symbol("keys1");
 const sym2 = Symbol("keys2");
 
 
-const jsUser = {//object literals
+let jsUser = {//object literals
  name : "amrita",
  [sym1] :"keys1",   //using square bracket in key means it is symbol 
  age : 18,
@@ -25,7 +25,7 @@ myArr[1]; //we can access array in this way bt cant access object
 // console.log(typeof jsUser[sym1]);   //string
 
 jsUser.email="3";  //change value of key
-console.log(jsUser);   
+//console.log(jsUser);   
 /*op --- {
   name: 'amrita',
   age: 18,
@@ -37,7 +37,7 @@ console.log(jsUser);
 
 Object.freeze(jsUser);      //freeze is func which allow to not make changes directly on main code
 jsUser.age = 20;   // trying to replace
-console.log(jsUser);
+//console.log(jsUser);
 /* 
 {
   name: 'amrita',
@@ -47,3 +47,42 @@ console.log(jsUser);
   email: '3',
   [Symbol(keys1)]: 'keys1'
 } */
+
+jsUser.newKey = "dev";
+//console.log(jsUser.newKey); //undefined bcoz we had freeze our object , we need to unfreeze it
+//console.log( Object.isFrozen(jsUser) );    //true 
+//so to unfreeze it : we can do two thing , first comment the Object.freeze(jsUser) , second way is to make new object and run for loop in jsUser and copy it in newObj and then assign (jsUser = newObj)
+
+//below is the second method to unfreeze jsUser
+
+let newObj = {};   //making a new obj
+
+for( var i in jsUser){
+  newObj[i] = jsUser[i];              //we copied all elements of jsUser to newObj
+}
+
+jsUser = newObj;   //we assign newObj to jsUser
+//console.log(Object.isFrozen(jsUser));   //op ----- false
+
+
+jsUser.greeting = function () {
+  console.log("hello "); 
+  }
+ //console.log(jsUser.greeting());    //hello  undefined
+ //console.log(jsUser.greeting);   //[Function (anonymous)]
+ //console.log(jsUser);
+ /*{
+  name: 'amrita',
+  age: 18,
+  location: 'jaipur',
+  lastLoginDay: [ 'mon ', 'sat' ],
+  email: '3',
+  greeting: [Function (anonymous)]
+} */
+
+jsUser.greeting2 = function (){
+ // console.log(`hello user ${jsUser.name}`);
+ console.log(`hello user ${this.name}`);  // same as above code
+} 
+  
+console.log(jsUser.greeting2());     //hello user amrita  undefined
---
+++ 02_basics/04_objects.js
@@ -0,0 +1,96 @@
+// object singleton or constructor
+
+//SINGLETON OBJECT :
+const obj1 = new Object();  // another way to declare obj
+//console.log(obj1);   //{}
+
+//NON SINGLETON OBJECT :
+const obj2= {};
+//console.log(obj2);   //{}
+
+//Adding keys to singleton obj - obj1
+obj1.id="123asd";
+obj1.name="dev";
+obj1.isLoggedIn = false;
+
+//console.log(obj1);   //{ id: '123asd', name: 'dev', isLoggedIn: false }
+
+const regularUser = {
+    email : "dev@gmail.com" ,
+    name : "dev",
+    fullName : {
+        userFullName : {
+        first : "shree",
+        middle : "dev",
+        last : "anand"
+    }
+}
+}
+//console.log(regularUser.fullName.userFullName);    //{ first: 'shree', middle: 'dev', last: 'anand' } , we can access nested object
+obj1.newKey = regularUser;  //can add new obj as key 
+//console.log(obj1);
+/*
+id: '123asd',
+  name: 'dev',
+  isLoggedIn: false,
+  newKey: {
+    email: 'dev@gmail.com',
+    name: 'dev',
+    fullName: { userFullName: [Object] }
+  }
+}*/
+
+const objA = {1:"a" , 2:"b"}
+const objB = {3:"c" , 4:"d"}
+
+//MERGING MORE OBJECTS IN ANOTHER OBJECT --- 1> ASSIGN METHOD  2> SPREAD METHOD
+
+//  --------------------------------    1>  ASSIGN       ------------------------------------------------------------
+
+const mergedObj = {objA,objB};   //it will make two objects as key in mergedObj 
+//console.log(mergedObj);  //{ objA: { '1': 'a', '2': 'b' }, objB: { '3': 'c', '4': 'd' } }
+
+const objAssignInNewObj_1 = Object.assign( {}/*---(target)*/, objA, objB /*---(source)*/)  
+//console.log(objAssignInNewObj_1);     //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+//console.log(objA);      //{ '1': 'a', '2': 'b' }
+
+const objAssignInNewObj = Object.assign(objA , objB);  //here , when objA is at first place , it becomes target so value of objA wil get chnage
+//console.log(objAssignInNewObj);     //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+//console.log(objA);    //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+
+//console.log( objAssignInNewObj == objA);  //true : returned obj == target
+
+//-----------------------------------      2> SPREAD  {Mostly Used Method Now-A-Days}  ------------------------------------------------------
+
+const a = {1:"b",2:"c",3:"d"};
+const b = {5:"e" , 6 : " f" , 7 :"g"};
+const spreadtoMerge = {...a , ...b};   //{ '1': 'b', '2': 'c', '3': 'd', '5': 'e', '6': ' f', '7': 'g' }
+//console.log(spreadtoMerge);
+
+// when data comes from database ,it looks like ----(basic)
+
+const users = [
+    {
+        id:1,
+        name : "dev"
+    },
+    {
+        id:2,
+        name : "devi"
+    }
+];
+//console.log(users[0].name); //dev : users[1] means acess the object which is at 0th idx in users arr , and in  users[1].name : users[1] is obj so to access key of obj we write : users[1].name
+//console.log(users);   //[ { id: 1, name: 'dev' }, { id: 2, name: 'devi' } ]
+
+
+//below code are very imp. for handling database
+const allKeysOfObj1 = Object.keys(obj1);
+//console.log(allKeysOfObj1);   //[ 'id', 'name', 'isLoggedIn', 'newKey' ] ,=it is array, can run loop easily , while handling database , we will use these keys of obj to run loop 
+//console.log(Object.values(objA));   //[ 'a', 'b', 'c', 'd' ] , all the values of keys in array format
+
+//console.log(Object.entries(objA));  //[ [ '1', 'a' ], [ '2', 'b' ], [ '3', 'c' ], [ '4', 'd' ] ] , all the individual keys,values become array and wrapped in arrray
+
+//when we want to find value in obj bt it does not exist ----we do this
+
+//console.log(obj1.hasOwnProperty('name'));  //true
+//console.log(objA.hasOwnProperty('name'));  //false
