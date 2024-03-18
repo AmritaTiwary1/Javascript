103d96600f68ff661ee26e4ae507421244ae3d3c
Amrita Tiwary <119927883+AmritaTiwary1@users.noreply.github.com>
2024-03-18 07:12:19 +0000

    scope_mediumLevel

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -4,11 +4,11 @@
 
 var scope_var = "outside_var";
 {
-    console.log(scope_var);   //outside_var
+   // console.log(scope_var);   //outside_var
     var scope_var = "inside_var";  //inside_var
-    console.log(scope_var);
+   // console.log(scope_var);
 }
-console.log(scope_var);  //inside_var
+//console.log(scope_var);  //inside_var
 
 
 //--------------------------------  LET VARIABle -----------------------------------------------
@@ -59,7 +59,51 @@ if(true){
 }
 //console.log( one ); //ERROR : one is not defined
 //console.log( two );   //ERROR : two is not dfined
-console.log( three );  //2345
+//console.log( three );  //2345
 
-//NOTE : ----------- variable declares as let or const inside block is not accessed outside of block bt var is global , whether it is declared inside or outside of block , it can be access from anywhere
+//NOTE ^^^: ----------- variable declares as let or const inside block is not accessed outside of block bt var is global , whether it is declared inside or outside of block , it can be access from anywhere
 
+function one(){
+    const username = "dev"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);  //ERROR : ReferenceError: website is not defined
+
+    two();
+}
+//one();    //------------------->>>> ERROR until we dont comment out console.log(website); , After commenting we get =========== dev
+
+// NOTE : child function/block can access parents variable but parent variable cant access child function/ block variable 
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+if(true ){
+    const username = "dev";
+    if(username === "dev"){
+        const website = "youtube"
+        //console.log(username , website);   //dev youtube
+    }
+   // console.log(website);  //ReferenceError: website is not defined  -----> parent block cant access child's block variable
+} 
+//console.log(username);  //ReferenceError: username is not defined ----> outside the block , username cant accessed bcoz parent block cant access child block 
+
+//++++++++++++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//1st syntax to write function
+
+//console.log(addone(3));   //4 ----> no error , we can access function before declaring and initialising func
+function addone(num) {
+    return num+1;
+}
+addone(5);    
+
+//2nd syntax -------
+
+console.log(addTwo(7));    //ReferenceError: Cannot access 'addTwo' before initialization
+const addTwo = function (num) {
+    return num+2;
+}
+addTwo(8);   // no input :  but the main thing is that a variable has hold function  
